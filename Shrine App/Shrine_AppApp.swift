591a1bdec124b67228905b0bd654eb225ba4627a
Jonathan A <jamadorh91@gmail.com>
2022-11-18 12:39:17 -0600

    Initial Commit

---
+++ Shrine App/Shrine_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shrine_AppApp.swift
+//  Shrine App
+//
+//  Created by JonathanA on 18/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Shrine_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
